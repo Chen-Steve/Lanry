f6b9c14e2fb60252a0391cb377c31450e8147a66
Steven <zhangc2@illinois.edu>
2024-11-13 12:50:39 -0600

    install vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,18 @@ import "./globals.css";
 import HeaderWrapper from '@/components/HeaderWrapper';
 import FooterWrapper from '@/components/FooterWrapper';
 import Providers from '@/components/Providers';
+import AnalyticsProvider from '@/components/AnalyticsProvider';
 import { Toaster } from 'react-hot-toast';
 
 export const metadata: Metadata = {
   title: "Lanry",
   description: "read light novel",
+  metadataBase: new URL('https://lanry.vercel.app'),
+  openGraph: {
+    title: 'Lanry',
+    description: 'read light novel',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
@@ -27,6 +34,7 @@ export default function RootLayout({
             <FooterWrapper />
           </div>
           <Toaster position="bottom-right" />
+          <AnalyticsProvider />
         </Providers>
       </body>
     </html>
