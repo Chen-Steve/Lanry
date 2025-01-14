a1cffc6d2be9498a48c818d92deec59ef7cccc44
Steven <zhangc2@illinois.edu>
2025-01-13 22:16:21 -0600

    setting up google adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Providers from './providers';
 import { Toaster } from 'react-hot-toast';
 import MobileNavigation from './_components/MobileNavigation';
 import { ThemeProvider } from '@/lib/ThemeContext';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: "Lanry",
@@ -34,6 +35,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1969691044413795"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className="min-h-screen relative">
         <Providers>
           <ThemeProvider>
