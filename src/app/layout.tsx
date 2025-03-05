261b47b551b14b2fd5c9e2e8b23085e558cd8b95
Steven <zhangc2@illinois.edu>
2025-03-05 12:44:20 -0600

    add banners to home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,15 +73,38 @@ export default function RootLayout({
           strategy="afterInteractive"
         />
       </head>
-      <body className="min-h-screen relative">
+      <body className="min-h-screen relative overflow-x-hidden">
         <Providers>
           <ThemeProvider>
             <AnalyticsWrapper>
-              <div className="flex flex-col min-h-screen">
+              <div className="flex flex-col min-h-screen max-w-[100vw]">
                 <Header />
-                <main className="flex-grow">
-                  {children}
-                </main>
+                <div className="flex-grow flex justify-between w-full">
+                  {/* Left Ad Column - Hidden on mobile */}
+                  <div className="hidden lg:block w-[120px] xl:w-[160px] flex-shrink-0 sticky top-0 h-screen">
+                    <div className="p-2">
+                      {/* Left Ad Container */}
+                      <div id="left-ad-container" className="w-full h-[400px]">
+                        {/* Your left ad unit code here */}
+                      </div>
+                    </div>
+                  </div>
+                  
+                  {/* Main Content */}
+                  <main className="flex-grow max-w-full overflow-x-hidden pr-2">
+                    {children}
+                  </main>
+                  
+                  {/* Right Ad Column - Hidden on mobile */}
+                  <div className="hidden lg:block w-[120px] xl:w-[160px] flex-shrink-0 sticky top-0 h-screen -ml-2">
+                    <div className="p-0">
+                      {/* Right Ad Container */}
+                      <div id="right-ad-container" className="w-full h-[400px]">
+                        {/* Your right ad unit code here */}
+                      </div>
+                    </div>
+                  </div>
+                </div>
                 <Footer />
               </div>
               <Toaster position="bottom-right" />
